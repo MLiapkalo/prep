113006f2b931f760ad898d41f4b0e03c098b70af
Maksym Liapkalo <Maksym_Liapkalo@epam.com>
2020-08-20 18:55:52 +0300

    728, 136

---
+++ src/leet/136.js
@@ -0,0 +1,28 @@
+/*
+    Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+    Note:
+    Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+    Example 1:
+    Input: [2,2,1]
+    Output: 1
+
+    Example 2:
+    Input: [4,1,2,1,2]
+    Output: 4
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = nums => {
+    const set = new Set();
+
+    for (let n of nums) {
+        set.has(n) ? set.delete(n) : set.add(n);
+    }
+
+    return Array.from(set)[0];
+};
